4676b80e6551d181d405e9040a97be9d68ae6dff
sogabrie <gabrielyan.sos@mail.ru>
2023-06-30 23:34:13 +0400

    update

--- Module_00/ex00/megaphone.cpp
+++ Module_00/ex00/megaphone.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 
+std::string form(std::string ptr)
+{
+    for (size_t i = 0; i < ptr.size(); i++)
+    {
+        if (ptr[i] > '`' && ptr[i] < '{')
+            ptr[i] = ptr[i] - 32;
+    }
+    return (ptr);
+}
+
 int main(int c, char **v)
 {
     if (c < 2)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     else
     {
-        std::cout << v[1] << std::endl;
+        --c;
+        ++v;
+        for (int i = 0; i < c; i++)
+            std::cout << form(v[i]);
+        
     }
+    std::cout << std::endl;
     return (0);
 }
