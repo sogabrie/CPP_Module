a7c7f4873b59603c7a8fa9356ef926febd0fbb02
sogabrie <gabrielyan.sos@mail.ru>
2023-07-04 02:47:40 +0400

    add

--- Module_01/ex04/main.cpp
+++ Module_01/ex04/main.cpp
@@ -5,18 +5,27 @@ int main (int c, char **v)
 {
 	(void)c;
 	(void)v;
-	std::string b;
-	char a[11];
-	a[10] = 0;
-	std::fstream myfile;
-	myfile.open ("example.txt");
+	std::string a("aaaaa");
+	std::string a2("bbbbbb");
+	std::string b("aaaaasdasdasdfsdfsdfaaaaa");
+	// char a[11];
+	// a[10] = 0;
+	// std::fstream myfile;
+	// myfile.open ("example.txt");
 	// myfile << "Writing this to a file.\n";
-	while(myfile.read(a,10))
-	{
-		// myfile.read();
+	// while(myfile.read(a,10))
+	// {
+	// 	// myfile.read();
 		
-		std::cout << a;
+	// 	std::cout << a;
+	// }
+	// myfile.close();
+	size_t pos = b.find(a);
+	while ( pos != std::string::npos ) {
+		b.erase(pos, a.length());
+		b.insert(pos, a2);
+		pos = b.find(a);
 	}
-	myfile.close();
+	std::cout << b << std::endl;
 	return 0;
 }
