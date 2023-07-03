ed9f70f0b9884828aa8853703ffe44dcbeb0155c
sogabrie <gabrielyan.sos@mail.ru>
2023-07-03 23:36:11 +0400

    update

--- Module_01/ex04/main.cpp
+++ Module_01/ex04/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <fstream>
 
-
-int main(int c, char **v)
+int main (int c, char **v) 
 {
-	return (0);
+	(void)c;
+	(void)v;
+	std::string b;
+	char a[11];
+	a[10] = 0;
+	std::fstream myfile;
+	myfile.open ("example.txt");
+	// myfile << "Writing this to a file.\n";
+	while(myfile.read(a,10))
+	{
+		// myfile.read();
+		
+		std::cout << a;
+	}
+	myfile.close();
+	return 0;
 }
