01c788df940ffa946fcafcc6b95c38177af15d61
sogabrie <gabrielyan.sos@mail.ru>
2023-07-15 01:04:48 +0400

    update

---
+++ Module_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef __SCALARCONVERTER_HPP__
+#define __SCALARCONVERTER_HPP__
+
+class ScalarConverter
+{
+private:
+    /* data */
+public:
+    ScalarConverter(/* args */);
+    ~ScalarConverter();
+};
+
+ScalarConverter::ScalarConverter(/* args */)
+{
+}
+
+ScalarConverter::~ScalarConverter()
+{
+}
+
+
+#endif
