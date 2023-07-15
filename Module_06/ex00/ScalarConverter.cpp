e177b41bc7143874b93f17953d5dda6d76e1f01d
sogabrie <gabrielyan.sos@mail.ru>
2023-07-15 17:11:50 +0400

    update

---
+++ Module_06/ex00/ScalarConverter.cpp
@@ -0,0 +1,13 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{
+}
+
+ScalarConverter::~ScalarConverter()
+{
+}
+
+void ScalarConverter::convert(std::string str)
+{
+}
--- Module_06/ex00/ScalarConverter.hpp
+++ Module_06/ex00/ScalarConverter.hpp
@@ -2,22 +2,48 @@
 #ifndef __SCALARCONVERTER_HPP__
 #define __SCALARCONVERTER_HPP__
 
+#include <iostream>
+
+enum TYPE_L
+{
+	CHAR,
+	INT,
+	FLOAT,
+	DOUBLE
+}
+
 class ScalarConverter
 {
 private:
-    /* data */
+    ScalarConverter();
+
+	static TYPE_L	getType(std::string str);
+	static int		getInt(std::string str);
+	static char		getChar(std::string str);
+	static float	getFloat(std::string str);
+	static double	getDouble(std::string str);
+
+	static int		CharToInt(char str);
+	static int		FloatToInt(float str);
+	static int		DoubleToInt(double str);
+
+	static char		IntToChar(int str);
+	static char		FloatToChar(float str);
+	static char		DoubleToChar(double str);
+
+	static float	IntleToFloat(int str);
+	static float	CharToFloat(char str);
+	static float	DoubleToFloat(double str);
+
+	static double	IntleToDouble(int str);
+	static double	CharToDouble(char str);
+	static double	FloatToDouble(float str);
+	
 public:
-    ScalarConverter(/* args */);
     ~ScalarConverter();
+
+	static void convert(std::string str);
 };
-
-ScalarConverter::ScalarConverter(/* args */)
-{
-}
-
-ScalarConverter::~ScalarConverter()
-{
-}
 
 
 #endif
