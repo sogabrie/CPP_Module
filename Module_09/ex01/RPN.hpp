a0ced8a90c2903c42dbdbc4cf7e24bd243e1ed5d
sogabrie <gabrielyan.sos@mail.ru>
2023-07-29 00:50:22 +0400

    update

--- Module_09/ex01/RPN.hpp
+++ Module_09/ex01/RPN.hpp
@@ -2,10 +2,27 @@
 #ifndef __RPN_HPP__
 #define __RPN_HPP__
 
+#include <iostream>
+#include <stack>
+
 class RPN
 {
 private:
+
+	typedef enum s_TYPE
+	{
+	}		TYPE;
+
+	typedef struct s_type
+	{
+		TYPE _type;
+		double number;
+	}	t_type;
+
+	std::stack<t_type> _my_stack;
+
 public:
+
 	RPN();
 	~RPN();
 };
